22b1d6b2b37d6b7135e2c07f10122d42f99d7cca
riverprawn <greensaltfish@hotmail.com>
2023-03-03 23:41:19 +0800

    feat: added batch operations.

--- contracts/interfaces/pool/ICallPoolActions.sol
+++ contracts/interfaces/pool/ICallPoolActions.sol
@@ -7,11 +7,16 @@ interface ICallPoolActions {
     // Deposit NFT
     function deposit(address onBehalfOf, uint256 tokenId) external;
 
+    function depositBatch(address onBehalfOf, uint256[] memory tokenIds) external returns (uint256[] memory errorCodes);
+
     function depositWithPreference(address onBehalfOf, uint256 tokenId, uint8 lowerStrikePriceGapIdx, uint8 upperDurationIdx, uint256 minimumStrikePrice) external;
 
+    function depositWithPreferenceBatch(address onBehalfOf, uint256[] memory tokenIds, uint8[] memory lowerStrikePriceGapIdxList, uint8[] memory upperDurationIdxList, uint256[] memory minimumStrikePriceList) external returns(uint256[] memory errorCodes);
 
     // Withdraw NFT
     function withdraw(address to, uint256 tokenId) external;
+
+    function withdrawBatch(address to, uint256[] memory tokenIds) external returns (uint256[] memory errorCodes);
 
     function withdrawETH(address to, uint256 amount) external returns(uint256);
 
@@ -23,9 +28,16 @@ interface ICallPoolActions {
     // Close option
     function exerciseCall(uint256 tokenId) external payable;
 
+    function exerciseCallBatch(uint256[] memory tokenIds) external payable returns (uint256[] memory errorCodes);
+
     function takeNFTOffMarket(uint256 tokenId) external;
 
+    function takeNFTOffMarketBatch(uint256[] memory tokenIds) external returns (uint256[] memory errorCodes);
+
     function relistNFT(uint256 tokenId) external;
+
+    function relistNFTBatch(uint256[] memory tokenIds) external returns (uint256[] memory errorCodes);
+
 
     function changePreference(
         uint256 tokenId,
@@ -33,4 +45,11 @@ interface ICallPoolActions {
         uint8 upperDurationIdx,
         uint256 minimumStrikePrice
     ) external;
+
+    function changePreferenceBatch(
+        uint256[] memory tokenId,
+        uint8[] memory lowerStrikePriceGapIdxList,
+        uint8[] memory upperDurationIdxList,
+        uint256[] memory minimumStrikePriceList
+    ) external returns (uint256[] memory errorCodes);
 }
